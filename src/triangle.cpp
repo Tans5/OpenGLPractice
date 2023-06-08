93031e55593607ff1785793111e4de09369cbcbf
tanpengcheng <tanpengcheng@inke.cn>
2023-06-08 15:41:24 +0800

    [feat] 添加多个三角形绘制.

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -96,9 +96,10 @@ void drawTriangle() {
 
     // 坐标是从 -1 到 1
     float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f
+            0.5f, 0.5f, 0.0f,   // 右上角
+            0.5f, -0.5f, 0.0f,  // 右下角
+            -0.5f, -0.5f, 0.0f, // 左下角
+            -0.5f, 0.5f, 0.0f,   // 左上角
     };
 
     // 顶点缓冲对象：Vertex Buffer Object，VBO
@@ -108,6 +109,11 @@ void drawTriangle() {
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
+
+    // 元素缓冲对象：Element Buffer Object，EBO 或 索引缓冲对象 Index Buffer Object，IBO
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+
 
     // 绑定数据到 VBO
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -119,6 +125,25 @@ void drawTriangle() {
 
     // 使用编译好的渲染程序
     glUseProgram(programGlobal);
+//    glBindVertexArray(VAO);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // 绘制，第一个参数表示 图元，第二个参数索引开始位置，第三个参数顶点个数
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    unsigned int indices[] = {
+            // 注意索引从0开始!
+            // 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+            // 这样可以由下标代表顶点组合成矩形
+            0, 1, 3, // 第一个三角形
+            1, 2, 3,  // 第二个三角形
+            4, 2, 3,
+    };
+
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // 不填充，
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
 }
