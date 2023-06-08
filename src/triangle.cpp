ca51fdead6fe06e9a61331579e298431efc00dff
tanpengcheng <tanpengcheng@inke.cn>
2023-06-08 16:53:04 +0800

    [feat] 修改颜色生成.

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -7,22 +7,27 @@
 #include "GLFW/glfw3.h"
 #include "stdio.h"
 #include "triangle.h"
-#include "OpenGL/OpenGL.h"
+#include "math.h"
 
 // 顶点着色器 GLSL 源码
 const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "layout (location = 0) in vec3 aPos;\n" // 位置, 顶点着色器固定要加上 layout (location = 0)
+                                 "layout (location = 1) in vec3 aColor;\n" // 颜色
+                                 "out vec4 pColor;\n"
                                  "void main()\n"
                                  "{\n"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "   pColor = vec4(aColor, 1.0);\n"
                                  "}\0";
 
 // 片段着色器 GLSL 源码
 const char *fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
+                                   "in vec4 pColor;\n"
+                                   "uniform vec4 myColor = vec4(0.0, 1.0, 0.0, 1.0);\n" // 全局变量
+                                   "out vec4 FragColor;\n" // 输出变量
                                    "void main()\n"
                                    "{\n"
-                                   "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "    FragColor = myColor;\n"
                                    "}\n";
 
 const GLuint FAIL = -1;
@@ -96,10 +101,11 @@ void drawTriangle() {
 
     // 坐标是从 -1 到 1
     float vertices[] = {
-            0.5f, 0.5f, 0.0f,   // 右上角
-            0.5f, -0.5f, 0.0f,  // 右下角
-            -0.5f, -0.5f, 0.0f, // 左下角
-            -0.5f, 0.5f, 0.0f,   // 左上角
+            // 坐标(position 0)  // 颜色 (position 1)
+            0.5f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   // 右上角
+            0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 右下角
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,   // 左下角
+            -0.5f, 0.5f, 0.0f,  0.5f, 0.5f, 0.5f,   // 左上角
     };
 
     // 顶点缓冲对象：Vertex Buffer Object，VBO
@@ -120,11 +126,23 @@ void drawTriangle() {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // 设置顶点属性
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    // 坐标
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
+    // 颜色
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // 使用编译好的渲染程序
     glUseProgram(programGlobal);
+    // 更新uniform颜色
+    float timeValue = glfwGetTime();
+    float redValue = cos(timeValue) / 2.0f + 0.5f;
+    float greenValue = sin(timeValue) / 2.0f + 0.5f;
+    float blueValue = redValue + greenValue / 2;
+    int vertexColorLocation = glGetUniformLocation(programGlobal, "myColor");
+    glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
+
 //    glBindVertexArray(VAO);
 
     // 绘制，第一个参数表示 图元，第二个参数索引开始位置，第三个参数顶点个数
@@ -136,9 +154,7 @@ void drawTriangle() {
             // 这样可以由下标代表顶点组合成矩形
             0, 1, 3, // 第一个三角形
             1, 2, 3,  // 第二个三角形
-            4, 2, 3,
     };
-
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
