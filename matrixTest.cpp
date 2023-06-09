88ad41466995d4a669d851844446b96f1ed1e5fd
tanpengcheng <tanpengcheng@inke.cn>
2023-06-09 15:41:41 +0800

    [feat] 添加 GLM 矩阵，向量库.

---
+++ matrixTest.cpp
@@ -0,0 +1,12 @@
+//
+// Created by pengcheng.tan on 2023/6/9.
+//
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "stdio.h"
+
+int main() {
+
+    return 0;
+}
